c49ad128ff593682765f121248b02f85df15a360
Ronald <ronalanto03@gmail.com>
2022-02-21 19:27:00 -0800

    first commit

---
+++ AndGate/AndGate.cpp
@@ -0,0 +1,15 @@
+#include "AndGate.h"
+
+AndGate::AndGate(sc_module_name moduleName) : 
+  sc_module(moduleName),
+  aIn("aIn"),
+  bIn("bIn"),
+  cOut("cOut") {
+  SC_METHOD(process);
+  dont_initialize();
+  sensitive << aIn << bIn;
+}
+
+void AndGate::process() {
+  cOut.write(aIn.read() and bIn.read());
+}
---
+++ AndGate/AndGate.h
@@ -0,0 +1,18 @@
+#ifndef AND_GATE_H
+#define AND_GATE_H
+
+#include <systemc.h>
+
+class AndGate : public sc_module {
+  public:
+    sc_in<bool> aIn, bIn;
+    sc_out<bool> cOut;
+
+    SC_CTOR(AndGate);
+
+  private:
+    void process();
+
+};
+
+#endif // AND_GATE_H
